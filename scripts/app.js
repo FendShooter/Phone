376ffdbc0806fbe57f3df3f343bbceb939b96b2b
FendShooter <virgile.dokouvi@gmail.com>
2019-08-08 13:58:44 -0700

    working

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+
+import { Phone } from './class.js';
+// varaible
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".jsDisplay").focus();
+    phone.show();
+  phone.del();
+  // Phone.fetchData()
+});
+const phone = new Phone();
+
+
