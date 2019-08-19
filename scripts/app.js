47aa893cb6d6ab59996d9a3608164acb3a0aff6f
FendShooter <virgile.dokouvi@gmail.com>
2019-08-18 17:04:55 -0700

    this will add contact section

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,30 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".jsDisplay").focus();
   phone.fetchData();
   phone.del();
-  
-
 });
 const phone = new Phone();
 
+//variables
+
+const searchContact = document.querySelector("#searchContact");
+
+searchContact.addEventListener("keyup", filter);
+
+function filter() {
+    let value = searchContact.value.toUpperCase();
+
+    //looping through
+    let contacts = document.querySelectorAll('.name');
+    contacts.forEach(contact => {
+        const fil = contact;
+
+        if (fil.innerHTML.toUpperCase().indexOf(value) > -1) {
+            fil.style.display = 'block';
+        }
+        else {
+            fil.style.display = 'none';
+
+        }
+    });
+
+}
